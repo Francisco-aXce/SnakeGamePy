784a37bff9f7c3eb1890eec8aa5b0d50eee2867e
Francisco Ramos <jorge.ramos.francisco.15@gmail.com>
2021-01-05 23:17:51 -0300

    window scaled and snake logic

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import pygame, sys
 SCREEN_SIZE = (800,800)
 FILL_COLOR = (144,100,10)
 
+def drawSnake(snake, color = (0,0,0)):
+    for piece in snake:
+        pygame.draw.rect(main_sf, color, square)
+
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
+main_sf = pygame.Surface((50,50))
+main_sf.fill((255,255,255))
+clock = pygame.time.Clock()
 
 # Variables
+square = pygame.Rect(2,2,1,1)
+snake = [square]
 
 while True:
     for event in pygame.event.get():
@@ -15,4 +24,8 @@ while True:
 
     screen.fill(FILL_COLOR)
 
+    drawSnake(snake)
+    screen.blit(pygame.transform.scale(main_sf, SCREEN_SIZE), (0,0))
+
     pygame.display.update()
+    clock.tick(120)
